a41a3cac326a47930e20b5367dc54b0693ead7c7
Pat Allan <pat@freelancing-gods.com>
2013-10-22 16:53:36 +1100

    Validations to avoid database errors for points.

--- app/models/carmack/point.rb
+++ app/models/carmack/point.rb
@@ -3,4 +3,9 @@ class Carmack::Point < ActiveRecord::Base
 
   belongs_to :user,     polymorphic: true
   belongs_to :gameable, polymorphic: true
+
+  validates :context,    presence: true
+  validates :identifier, presence: true
+  validates :amount,     presence: true
+  validates :user,       presence: true
 end
