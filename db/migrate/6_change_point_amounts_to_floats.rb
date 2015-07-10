30675531ff7a518c14585871e68caf284b9c1d86
Pat Allan <pat@freelancing-gods.com>
2015-07-10 17:28:47 +1000

    Scale matters, and we don't want a default of zero.

--- db/migrate/6_change_point_amounts_to_floats.rb
+++ db/migrate/6_change_point_amounts_to_floats.rb
@@ -1,6 +1,10 @@
 class ChangePointAmountsToFloats < ActiveRecord::Migration
-  def change
+  def up
     change_column :scorecard_points, :amount, :decimal, null: false,
-      precision: 10
+      precision: 10, scale: 6
+  end
+
+  def down
+    change_column :scorecard_points, :amount, :integer
   end
 end
