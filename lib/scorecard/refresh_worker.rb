4f69e2d60a6ca8c78946872b644377409574f50f
Nathan S <nathan@inspire9.com>
2016-03-20 19:30:47 -0700

    add uniqueness to refresh

--- lib/scorecard/refresh_worker.rb
+++ lib/scorecard/refresh_worker.rb
@@ -1,5 +1,6 @@
 class Scorecard::RefreshWorker
   include Sidekiq::Worker
+  sidekiq_options unique_for: 15.minutes
 
   def perform(options)
     Scorecard::Scorer.refresh Scorecard::Parameters.new(options).contract
